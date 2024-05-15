15d2035e7168e37ab4ee1acc87b1e4d72ca35a12
Adjel <adjel.touaoula@gmail.com>
2024-05-15 13:28:46 +0200

    feat(todoList/todoItem): able to create and load data

--- src/pages/TodoList.js
+++ src/pages/TodoList.js
@@ -1,5 +1,56 @@
 import Disconnect from "@/components/Disconnect";
+import TodoItem from "@/components/TodoItem";
+import { TodoContext } from "@/provider/TodoProvider";
+import { useContext, useEffect, useState } from "react";
 
 export default function TodoList() {
-  return <Disconnect />;
+  const [todo, setTodo] = useState({
+    title: "",
+    isCompleted: false,
+  });
+
+  const { todos, handleTodoInput } = useContext(TodoContext);
+
+  function handleTodo(event) {
+    const { name, value, checked } = event.target;
+    setTodo({
+      ...todo,
+      [name]: name === "isCompleted" ? checked : value,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    handleTodoInput(todo);
+  }
+
+  return (
+    <>
+      {todos.map(({ id, title, isCompleted }) => (
+        <TodoItem id={id} title={title} isCompleted={isCompleted} />
+      ))}
+      <form>
+        <label htmlFor="title">Title:</label>
+        <input
+          type="title"
+          id="title"
+          name="title"
+          value={todo.title}
+          onChange={(event) => handleTodo(event)}
+        ></input>
+        <label htmlFor="isCompleted">Completed:</label>
+        <input
+          type="checkbox"
+          id="isCompleted"
+          name="isCompleted"
+          value={todo.isCompleted}
+          onChange={(event) => handleTodo(event)}
+        ></input>
+        <button type="submit" onClick={(event) => handleSubmit(event)}>
+          Create
+        </button>
+      </form>
+      <Disconnect />
+    </>
+  );
 }
--- src/provider/TodoProvider.js
+++ src/provider/TodoProvider.js
@@ -1,7 +1,48 @@
-import { createContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { addDoc, collection, db, onSnapshot } from "../Firebase";
+import { UserContext } from "./UserProvider";
 
 export const TodoContext = createContext();
 
 export default function TodoProvider({ children }) {
-  return <TodoContext.Provider value="">{children}</TodoContext.Provider>;
+  const [todos, setTodos] = useState([]);
+
+  const { user } = useContext(UserContext);
+
+  useEffect(() => {
+    if (user) {
+      const ref = collection(db, "users", user.uid, "todos");
+      const unsubscribe = onSnapshot(ref, (querySnapshot) => {
+        const allTodos = [];
+        querySnapshot.forEach((doc) => {
+          allTodos.push({
+            id: doc.id,
+            ...doc.data(),
+          });
+        });
+        setTodos(allTodos);
+        console.log("todos loeaded");
+      });
+
+      return () => {
+        unsubscribe();
+      };
+    }
+  }, [user]);
+
+  async function handleTodoInput({ title, isCompleted }) {
+    const ref = collection(db, "users", user.uid, "todos");
+    const docRef = await addDoc(ref, {
+      title: title,
+      completed: isCompleted,
+    });
+    console.log({ user });
+    console.log({ docRef });
+  }
+
+  return (
+    <TodoContext.Provider value={{ todos, handleTodoInput }}>
+      {children}
+    </TodoContext.Provider>
+  );
 }
